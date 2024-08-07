d79245fa4da64d83b43e6b1ab1cef5d83162f342
Ece Yurtseven <35323206-swimmerece@users.noreply.replit.com>
2024-08-07 12:34:36 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+//Press a button to choose your path
+//See the README file for more information
+
+/* VARIABLES */
+let enterButton;
+let a1Button;
+let a2Button;
+let b1Button;
+let b2Button;
+let screen = 0;
+
+/* SETUP RUNS ONCE */
+function setup() {
+  createCanvas(600, 400);
+  textAlign(CENTER);
+  textSize(20);
+  noStroke();
+
+  // Set up the home screen
+  background("pink");
+  text(
+    "Welcome to screen 0. This is the home screen.",
+    width / 2,
+    height / 2 - 100
+  );
+
+  // Create buttons for all screens
+
+}
+
+/* DRAW LOOP REPEATS */
+function draw() {
+  // Display enter button
+
+
+  // Add A1 button
+
+
+  // Add A2 button
+
+
+  // Check enter button
+
+}
+
+/* FUNCTIONS TO DISPLAY SCREENS */
